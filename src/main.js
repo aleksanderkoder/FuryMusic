365beb2e220381830470f08d081730effab2e1db
aleksanderkoder <neon.113@hotmail.com>
2020-12-14 03:36:38 +0100

    Security repo changes

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faUser,faLock,faUserSecret,faAt,faPlay,faPause} from '@fortawesome/free-solid-svg-icons'
 
+
 library.add(faUser,faLock,faUserSecret,faAt,faPlay,faPause)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
