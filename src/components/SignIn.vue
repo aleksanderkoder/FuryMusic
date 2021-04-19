a80c5819b07c02834755dbd01e259ebe5fdb55d4
aleksanderkoder <neon.113@hotmail.com>
2021-04-19 22:45:23 +0200

    Responsive CSS for higher than 2K screen resolutions

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -3,14 +3,13 @@
     <div class="animate__animated animate__fadeInDownBig" id="divLogInWrapper">
       <img
         id="logo"
-        width="250"
         class="animate__animated animate__jackInTheBox animate__slow"
         src="../assets/fury music logo ferdig.png"
       />
       <br />
       <h2 id="logInTittel">Sign in</h2>
       <form onSubmit="return false">
-        <font-awesome-icon style="color: black" :icon="['fas', 'user']" />
+        <font-awesome-icon class="fontAwesome" style="color: black" :icon="['fas', 'user']" />
         <input
           type="text"
           v-model="username"
@@ -18,7 +17,7 @@
           id="username"
         />
         <br />
-        <font-awesome-icon style="color: black" :icon="['fas', 'lock']" />
+        <font-awesome-icon class="fontAwesome" style="color: black" :icon="['fas', 'lock']" />
         <input
           type="password"
           v-model="password"
@@ -180,136 +179,219 @@ export default {
 </script>
 
 <style scoped>
+
 #pageWrapper {
-  height: 100%;
-  width: 100%;
-  display: flex;
-  justify-content: center;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
+
+  #logo {
+    width: 250px; 
+  }
+
+  #divLogInWrapper {
+    text-align: center;
+    color: #2c3e50;
+    background-color: rgba(255, 255, 255, 1);
+    width: 400px;
+    min-height: 670px;
+    margin: auto;
+    /* margin-top: 6.5%; */
+    border-radius: 1%;
+    padding: 20px;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    font-family: "Monsterrat";
+  }
+
+  #loadingSpinner {
+    color: black;
+    font-size: 40px;
+    display: none;
+    margin: auto;
+  }
+
+  #username,
+  #password {
+    border: none;
+    margin: 15px;
+    margin-right: 33px;
+    width: 155px;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 2px 4px;
+    height: 30px;
+    font-family: "Wals";
+    border-radius: 3px;
+  }
+
+  #error {
+    color: red;
+    display: none;
+    font-family: "Monsterrat";
+  }
+
+  #btnLogIn {
+    margin: auto;
+    margin-top: 20px;
+    background-color: black;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    color: white;
+    text-decoration: none;
+    border: none;
+    height: 35px;
+    width: 140px;
+    border-radius: 4px;
+    font-family: "Wals";
+    transition: 0.3s;
+    cursor: pointer;
+  }
+
+  #btnGoToSignUp {
+    margin: auto;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    background-color: white;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    color: black;
+    text-decoration: none;
+    border: none;
+    height: 35px;
+    width: 140px;
+    border-radius: 4px;
+    font-family: "Wals";
+    transition: 0.3s;
+  }
+
+  #btnGoToSignUp:hover {
+    background-color: lightgrey;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  }
+
+  #btnLogIn:hover {
+    background-color: #2022259f;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  }
+
+  #makersMark {
+    position: absolute;
+    display: block;
+    text-align: center;
+    width: 100%;
+    bottom: 0;
+    color: white;
+  }
+
+  #logInTittel {
+    border-bottom: 1px solid;
+    width: 75px;
+    margin: 20px auto;
+    font-family: "Wals";
+    color: black;
+  }
+
+  .fontAwesome {
+    font-size: 16px;
+  }
+
+  button {
+    outline: none;
+    cursor: pointer;
+  }
+
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    display: block;
+    color: black;
+    font-size: 12px;
+    font-family: "Monsterrat", sans-serif;
+    margin-top: 25px;
+  }
+
+  @media screen and (min-width: 3840px) {
+ 
+#logo {
+  width: 500px;
 }
 
-#divLogInWrapper {
-  text-align: center;
-  color: #2c3e50;
-  background-color: rgba(255, 255, 255, 1);
-  width: 400px;
-  min-height: 670px;
-  margin: auto;
-  /* margin-top: 6.5%; */
-  border-radius: 1%;
-  padding: 20px;
-  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-  font-family: "Monsterrat";
+ #divLogInWrapper {
+    width: 800px;
+    min-height: 1340px;
+    padding: 40px;
+  }
+
+  #loadingSpinner {
+    font-size: 80px;
+  }
+
+  #username,
+  #password {
+    margin: 30px;
+    margin-right: 66px;
+    width: 310px;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 4px 8px;
+    font-size: 24px; 
+    height: 60px;
+  }
+
+  #btnLogIn {
+    margin-top: 20px;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 6px 16px;
+    height: 70px;
+    width: 280px;
+    font-size: 24px;
+  }
+
+  #btnGoToSignUp {
+    margin-top: 40px;
+    margin-bottom: 40px;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 6px 16px;
+    height: 70px;
+    width: 280px;
+    font-size: 24px;
+  }
+
+  #btnGoToSignUp:hover {
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 6px 16px;
+  }
+
+  #btnLogIn:hover {
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 6px 16px;
+  }
+
+  #makersMark {
+    font-size: 24px; 
+  }
+
+  #logInTittel {
+    border-bottom: 2px solid;
+    width: 150px;
+    margin: 40px auto;
+    font-size: 48px;
+  }
+
+  a {
+    margin-top: 40px; 
+    font-size: 24px; 
+  }
+
+  .fontAwesome {
+    font-size: 32px;
+  }
+
 }
 
-#loadingSpinner {
-  color: black;
-  font-size: 40px;
-  display: none;
-  margin: auto;
-}
-
-#username,
-#password {
-  border: none;
-  margin: 15px;
-  margin-right: 33px;
-  width: 155px;
-  box-shadow: rgba(0, 0, 0, 0.24) 0px 2px 4px;
-  height: 30px;
-  font-family: "Wals";
-  border-radius: 3px;
-}
-
-#error {
-  color: red;
-  display: none;
-  font-family: "Monsterrat";
-}
-
-#btnLogIn {
-  margin: auto;
-  margin-top: 20px;
-  background-color: black;
-  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-  color: white;
-  text-decoration: none;
-  border: none;
-  height: 35px;
-  width: 140px;
-  border-radius: 4px;
-  font-family: "Wals";
-  transition: 0.3s;
-  cursor: pointer;
-}
-
-#btnGoToSignUp {
-  margin: auto;
-  margin-top: 20px;
-  margin-bottom: 20px;
-  background-color: white;
-  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-  color: black;
-  text-decoration: none;
-  border: none;
-  height: 35px;
-  width: 140px;
-  border-radius: 4px;
-  font-family: "Wals";
-  transition: 0.3s;
-}
-
-#btnGoToSignUp:hover {
-  background-color: lightgrey;
-  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-}
-
-#btnLogIn:hover {
-  background-color: #2022259f;
-  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-}
-
-#makersMark {
-  position: absolute;
-  display: block;
-  text-align: center;
-  width: 100%;
-  bottom: 0;
-  color: white;
-}
-
-#logInTittel {
-  border-bottom: 1px solid;
-  width: 75px;
-  margin: 20px auto;
-  font-family: "Wals";
-  color: black;
-}
-
-button {
-  outline: none;
-  cursor: pointer;
-}
-
-h1,
-h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  display: block;
-  color: black;
-  font-size: 12px;
-  font-family: "Monsterrat", sans-serif;
-  margin-top: 25px;
-}
 </style>
