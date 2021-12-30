8623f67e3d357abadd2495349245d34aeb77bb9d
aleksanderkoder <neon.113@hotmail.com>
2021-12-30 23:49:52 +0100

    Change pictures

--- src/App.vue
+++ src/App.vue
@@ -62,7 +62,7 @@ export default {
   right: 0;
   bottom: 0;
   margin: auto;
-  background: url("./assets/def back3.webp") no-repeat center center
+  background: url("./assets/def-back.webp") no-repeat center center
     fixed;
   -webkit-background-size: cover;
   -moz-background-size: cover;
--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -258,7 +258,7 @@ export default {
   grid-column-start: 1;
   grid-column-end: 1;
   grid-row-start: 1;
-  background: url("../assets/polina-silivanova-42_l9l5Pxwk-unsplash.webp")
+  background: url("../assets/def-back-login.webp")
     no-repeat center center fixed;
   -webkit-background-size: cover;
   -moz-background-size: cover;
